f93c7124f84321e31853b3677f18fcff75a4ce2f
Tacohontas <Jonathan.undhagen@gmail.com>
2019-11-22 16:05:45 +0100

    Javascript till invoice
    1. Har sett till att all input-data hamnar på invoice.
    
    2. Sett till att invoice genererar datum och ordernummer. (JAJJAMEN!!!!)

--- js/invoiceTEST.js
+++ js/invoiceTEST.js
@@ -1,11 +1,42 @@
 function ShowDataInvoice() {
-  const nameDiv = document.querySelector(".namediv"); // Döp en variabel till en selector som väljer platsen du vill stoppa in info.
-  const phoneDiv = document.querySelector(".phonediv");
+  // Döp en variabel till en queryselector som väljer platsen du vill stoppa in infon i.
 
-  // syntaxen här är:
-  // variabelnamn.innerHTML = localStorage.getItem("namnet du satt på ditt item");
-  nameDiv.innerHTML = localStorage.getItem("namn");
-  phoneDiv.innerHTML = localStorage.getItem("telefon");
+  // Section1
+  const businessNameDiv = document.querySelector(".business_name_div");
+  const streetDiv = document.querySelector(".street_div");
+  const zipDiv = document.querySelector(".zip_div");
+  const contactNameDiv = document.querySelector(".contact_name_div");
+  const contactPhoneDiv = document.querySelector(".contact_phone_div");
+  const contactEmailDiv = document.querySelector(".contact_email_div");
+  const messageDiv = document.querySelector(".message_div");
+
+  // Section2
+  const dateDiv = document.querySelector(".date_div");
+  const invoiceNumberDiv = document.querySelector(".invoicenumber_div");
+
+  /* Här skriver vi in våran hämtade data från shoppingcart.html till div-boxar i den nuvarande html-filen. 
+  
+  syntaxen här är:
+  variabelnamn.innerHTML = localStorage.getItem("namnet du satt på ditt localStorage-item");
+  
+  */
+  businessNameDiv.innerHTML = localStorage.getItem("business_name");
+  streetDiv.innerHTML = localStorage.getItem("street");
+  zipDiv.innerHTML = localStorage.getItem("zip");
+  contactNameDiv.innerHTML = localStorage.getItem("contact_name");
+  contactPhoneDiv.innerHTML = localStorage.getItem("contact_phone");
+  contactEmailDiv.innerHTML = localStorage.getItem("contact_email");
+  messageDiv.innerHTML = localStorage.getItem("message");
+
+  // Generera dagens datum.
+  thisYear = new Date().getFullYear();
+  thisMonth = new Date().getMonth() + 1; // +1 för att 0 = Januari.
+  thisDate = new Date().getDate();
+  dateDiv.innerHTML = `${thisYear} - ${thisMonth} - ${thisDate}`;
+
+  // Randomisera fakturanummer
+  // (Genererar nu ett nummer mellan 100000-900000)
+  invoiceNumberDiv.innerHTML = Math.floor(Math.random() * 900000) + 100000;
 }
 
 // När HELA sidan laddats klart så körs ShowDataInvoice-funktionen:
@@ -13,11 +44,10 @@ document.addEventListener("DOMContentLoaded", function() {
   ShowDataInvoice();
 });
 
-// Tillbaka-knapp
+// En Tillbaka-knapp för att komma tillbaks till shoppingcart (bara för att göra det lättare för oss att gå fram och tillbaka)
 const returnBtn = document.querySelector(".tillbakabtn");
-returnBtn.addEventListener("click", tillbakaTillVarukorg);
+returnBtn.addEventListener("click", tillbakaTillShoppingcart);
 
-function tillbakaTillVarukorg(){
-  window.document.location = "./shoppingcart.html"
+function tillbakaTillShoppingcart() {
+  window.document.location = "./shoppingcart.html";
 }
-
--- js/shoppingcart.js
+++ js/shoppingcart.js
@@ -6,6 +6,8 @@ document.addEventListener("DOMContentLoaded", function() {
 const addBtn = document.querySelector(".addDetails");
 addBtn.addEventListener("click", addInfo);
 
+
+// Hämtar produkt (produktens bild) från produkter.html 
 function ShowData() {
   const chosenproduct__product__img = document.querySelector(
     ".chosenproduct__product__img"
@@ -13,12 +15,24 @@ function ShowData() {
   chosenproduct__product__img.src = localStorage.getItem("data");
 }
 
+// Skickar vidare info från formen där man fyller i företagsuppgifter
 function addInfo() {
-  const formName = document.querySelector("#business_name").value;
-  const formPhone = document.querySelector("#contact_phone").value;
+  const businessNameValue = document.querySelector("#business_name").value;
+  const streetValue = document.querySelector("#street").value;
+  const zipValue = document.querySelector("#zip").value;
+  const contactNameValue = document.querySelector("#contact_name").value;
+  const contactPhoneValue = document.querySelector("#contact_phone").value;
+  const contactEmailValue = document.querySelector("#contact_email").value;
+  const messageValue = document.querySelector("#message").value;
 
-  localStorage.setItem("namn", formName);
-  localStorage.setItem("telefon", formPhone);
+  // Lagrar ovanstående i localstorage.
+  localStorage.setItem("business_name", businessNameValue);
+  localStorage.setItem("street", streetValue);
+  localStorage.setItem("zip", zipValue);
+  localStorage.setItem("contact_name", contactNameValue);
+  localStorage.setItem("contact_phone", contactPhoneValue);
+  localStorage.setItem("contact_email", contactEmailValue);
+  localStorage.setItem("message", messageValue);
 
   window.document.location = "../html/invoiceTEST.html";
 }
